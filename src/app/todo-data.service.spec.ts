5fa3b5f2900de730c33b309780a09d52386938e0
warawich torpithakpong <warawich_tor@truecorp.co.th>
2018-01-16 21:44:11 +0700

    add feature

---
+++ src/app/todo-data.service.spec.ts
@@ -0,0 +1,26 @@
+import { TestBed, inject } from '@angular/core/testing';
+import { Todo } from './todo';
+import { TodoDataService } from './todo-data.service';
+
+describe('TodoDataService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [TodoDataService]
+    });
+  });
+
+  it('should be created', inject([TodoDataService], (service: TodoDataService) => {
+    expect(service).toBeTruthy();
+  }));
+
+  it('shold ...', inject([TodoDataService], (service: TodoDataService) => {
+    expect(service).toBeTruthy();
+  }))
+
+  describe('#getAllTodos()', () => {
+    it('should return an empty array by default', inject([TodoDataService], (service: TodoDataService) => {
+      expect(service.getAllTodos()).toEqual([]);
+    }));
+  })
+
+});
